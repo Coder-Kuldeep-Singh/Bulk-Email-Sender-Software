da6bf0fb8361c63e7681c4afb18ef71bee7bcc52
mavensingh <kuldeep.avsar@gmail.com>
2020-06-23 19:26:31 +0530

    added controller to handle submittion of form

--- controllers/email.go
+++ controllers/email.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,4 +11,14 @@ func EmailController(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title": "GoSpike",
 	})
+}
+
+func EmailSenderController(c *gin.Context) {
+	_, err := c.MultipartForm()
+	if err != nil {
+		log.Println(err.Error())
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"formError": "Error to Parse Form",
+		})
+	}
 }
